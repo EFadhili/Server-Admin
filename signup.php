12ce9976df91dd8698c73dfeb27b3384b920ead6
Elton Fadhili <elton.mumalasi@strathmore.edu>
2024-10-03 02:56:57 +0300

    Updated signup and login pages

--- signup.php
+++ signup.php
@@ -7,12 +7,10 @@ $dbname = "students";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
